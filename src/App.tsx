d6516c69c5a5010d456836c50461540872f7ff1e
tahrerdgn53 <tahrerdgn53@gmail.com>
2026-07-29 14:12:01 +0300

    feat: Connect tool update handler to admin portal

--- src/App.tsx
+++ src/App.tsx
@@ -608,6 +608,7 @@ const handleAddNewsSubmit = async (newNews: any) => {
                 newsList={newsList}
                 onAddTool={handleAddToolSubmit}
                 onAddNews={handleAddNewsSubmit}
+                onUpdateTool={handleUpdateTool}
                 onDeleteTool={handleDeleteTool}
                 onDeleteNews={handleDeleteNews}
                 isAdmin={isAdmin}
